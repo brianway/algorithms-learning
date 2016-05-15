267ad544d387e883c2e83c373f31675e302b6d4e
brianway <250902678@qq.com>
2016-05-15 23:47:41 +0800

    202 Happy Number

---
+++ algorithms-leetcode/src/main/java/com/brianway/learning/algorithms/leetcode/easy/HappyNumber.java
@@ -0,0 +1,47 @@
+package com.brianway.learning.algorithms.leetcode.easy;
+
+import java.util.HashSet;
+
+/**
+ * Created by brian on 2016/5/15.
+ * LeetCode 202
+ * Question:https://leetcode.com/problems/happy-number/
+ * 关键题设： or it loops endlessly in a cycle which does not include 1
+
+ */
+public class HappyNumber {
+    public boolean isHappy(int n) {
+        return false;
+    }
+
+    /**
+     * 哈希表
+     * 结果总会出现环的，所以只需要每次都记录sum,每次算完sum查看是否已经存在
+     * 若存在则跳出循环，返回
+     * 若不存在则添加进HashSet，更新n
+     * TODO 时间复杂度 空间复杂度
+     */
+    public class HappyNumber0 extends HappyNumber{
+        @Override
+        public boolean isHappy(int n) {
+            HashSet<Integer> set = new HashSet<Integer>();
+            int sum =0;
+            while (true){
+                sum = 0;
+                while(n!=0){
+                    sum += (n%10)*(n%10);
+                    n=n/10;
+                }
+                if(set.contains(sum)){
+                    return sum==1;
+                }else{
+                    set.add(sum);
+                    n=sum;
+                }
+            }
+
+        }
+    }
+
+
+}
---
+++ algorithms-leetcode/src/test/java/com/brianway/learning/algorithms/leetcode/easy/HappyNumberTest.java
@@ -0,0 +1,33 @@
+package com.brianway.learning.algorithms.leetcode.easy;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Brian on 2016/4/30.
+ */
+public class HappyNumberTest {
+
+    private HappyNumber testObject;
+
+    int []nums = new int[]{19,11};
+
+    boolean [] result_array = new boolean[]{true,false};
+
+    @Before
+    public void SetUp(){
+        testObject =  new HappyNumber().new HappyNumber0();
+    }
+
+
+    @Test
+    public void testHappyNumberMore(){
+        for (int i=0;i<nums.length;i++){
+            Assert.assertEquals(result_array[i],testObject.isHappy(nums[i]));
+        }
+    }
+
+
+}
