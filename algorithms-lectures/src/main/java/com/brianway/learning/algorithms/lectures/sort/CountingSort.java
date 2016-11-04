f8224920ba7aa822b369cb365d30605b601ec3d3
brianway <250902678@qq.com>
2016-11-05 00:53:25 +0800

    [add] add counting sort and radix sort.

---
+++ algorithms-lectures/src/main/java/com/brianway/learning/algorithms/lectures/sort/CountingSort.java
@@ -0,0 +1,33 @@
+package com.brianway.learning.algorithms.lectures.sort;
+
+/**
+ * Created by brian on 16/11/4.
+ */
+public class CountingSort {
+    public int[] countingSort(int[] A, int n) {
+        if (A == null || n == 0) {
+            return null;
+        }
+
+        int min = A[0];
+        int max = A[0];
+        for (int i = 0; i < n; i++) {
+            min = Math.min(min, A[i]);
+            max = Math.max(max, A[i]);
+        }
+
+        int[] bucket = new int[max - min + 1];
+        for (int i = 0; i < n; i++) {
+            bucket[A[i] - min]++;
+        }
+        int i = 0;
+        for (int j = 0; j <= max - min; j++) {
+            for (int count = bucket[j]; count > 0; count--) {
+                A[i++] = j + min;
+            }
+        }
+
+        return A;
+    }
+
+}
---
+++ algorithms-lectures/src/main/java/com/brianway/learning/algorithms/lectures/sort/RadixSort.java
@@ -0,0 +1,41 @@
+package com.brianway.learning.algorithms.lectures.sort;
+
+/**
+ * Created by brian on 16/11/4.
+ *
+ * 保证元素均小于等于2000
+ */
+public class RadixSort {
+    private int[] radix = {1, 10, 100, 1000};
+
+    public int[] radixSort(int[] A, int n) {
+        if (A == null || n == 0) {
+            return null;
+        }
+        int total = 4;
+        int[][] bucket = new int[10][n];
+        int[] count = new int[10];
+        int number = 0;
+
+        for (int position = 0; position < total; position++) {
+            for (int i = 0; i < n; i++) {
+                number = getNumber(A[i], position);
+                bucket[number][count[number]++] = A[i];
+            }
+            int i = n - 1;
+            for (int k = 9; k >= 0; k--) {
+                while (count[k] > 0) {
+                    A[i--] = bucket[k][--count[k]];
+                }
+
+            }
+        }
+
+        return A;
+    }
+
+    private int getNumber(int num, int i) {
+        num = num / radix[i];
+        return num % 10;
+    }
+}
