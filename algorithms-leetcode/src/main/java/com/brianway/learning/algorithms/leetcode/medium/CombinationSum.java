2112fd5b55577657720acb1597ba5a5f05d4e90a
brianway <250902678@qq.com>
2022-09-26 23:59:02 +0800

    [update] LeetCode 39. Combination Sum排序剪枝

--- algorithms-leetcode/src/main/java/com/brianway/learning/algorithms/leetcode/medium/CombinationSum.java
+++ algorithms-leetcode/src/main/java/com/brianway/learning/algorithms/leetcode/medium/CombinationSum.java
@@ -1,6 +1,7 @@
 package com.brianway.learning.algorithms.leetcode.medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -19,7 +20,8 @@ public class CombinationSum {
     }
 
     /**
-     * 回溯+剪枝
+     * 回溯
+     * 注意，元素可重复选取， 所以回溯递归时，传参是i，而不是i+1
      */
     public class CombinationSum0 extends CombinationSum {
         @Override
@@ -39,6 +41,39 @@ public class CombinationSum {
             }
 
             for (int i = minIndex; i < candidates.length; i++) {
+                path.push(candidates[i]);
+                backtracking(result, path, pathSum + candidates[i], candidates, target, i);
+                path.pop();
+            }
+
+        }
+    }
+
+    /**
+     * 回溯+剪枝
+     * <p>
+     * 求和问题，可以排序后剪枝，优化性能
+     */
+    public class CombinationSum1 extends CombinationSum {
+        @Override
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            List<List<Integer>> result = new ArrayList<>();
+            // 排序candidates
+            Arrays.sort(candidates);
+            backtracking(result, new LinkedList<>(), 0, candidates, target, 0);
+            return result;
+        }
+
+        public void backtracking(List<List<Integer>> result, LinkedList<Integer> path, int pathSum,
+                                 int[] candidates, int target, int minIndex) {
+            if (pathSum >= target) {
+                if (pathSum == target) {
+                    result.add(new ArrayList<>(path));
+                }
+                return;
+            }
+
+            for (int i = minIndex; i < candidates.length && pathSum + candidates[i] <= target; i++) {
                 path.push(candidates[i]);
                 backtracking(result, path, pathSum + candidates[i], candidates, target, i);
                 path.pop();
